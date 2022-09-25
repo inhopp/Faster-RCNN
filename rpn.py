0295e62c54524051bf797148b1304193d22473fa
inhopp <inhonanananna@gmail.com>
2022-09-25 16:16:33 +0900

    make Region_Proposal_Network

--- rpn.py
+++ rpn.py
@@ -1,7 +1,8 @@
 import numpy as np
 import torch
+import torch.nn as nn
+from torch.nn import functional as F
 from torchvision.ops import nms
-from torchvision.ops import RoIPool
 
 def generate_anchor():
     base_size = 16
@@ -88,7 +89,7 @@ def bbox2loc(src_bbox, dst_bbox):
 class Proposal_Creator:
     '''generate RoI (train: 2000, test: 300)'''
 
-    def __init__(self, dev, parent_model,
+    def __init__(self, parent_model,
                  nms_thresh=0.7,
                  n_train_pre_nms=12000,
                  n_train_post_nms=2000,
@@ -96,7 +97,7 @@ class Proposal_Creator:
                  n_test_post_nms=300,
                  min_size=16):
         
-        self.dev = dev
+        self.parent_model = parent_model
         self.nms_thresh = nms_thresh
         self.n_train_pre_nms = n_train_pre_nms      # num of roi before nms
         self.n_train_post_nms = n_train_post_nms    # num of roi after nms
@@ -104,7 +105,7 @@ class Proposal_Creator:
         self.n_test_post_nms = n_test_post_nms      # num of roi after nms
         self.min_size = min_size
 
-    def __call__(self, loc, score, anchor, img_size, scale=1.):
+    def __call__(self, dev, loc, score, anchor, img_size, scale=1.):
         if self.parent_model.training:
             n_pre_nms = self.n_train_pre_nms
             n_post_nms = self.n_train_post_nms
@@ -136,8 +137,8 @@ class Proposal_Creator:
 
         # apply nms
         keep = nms(
-            torch.from_numpy(roi).to(self.dev),
-            torch.from_numpy(score).to(self.dev),
+            torch.from_numpy(roi).to(dev),
+            torch.from_numpy(score).to(dev),
             self.nms_thresh)
 
         if n_post_nms > 0:
@@ -146,3 +147,85 @@ class Proposal_Creator:
         roi = roi[keep.cpu().numpy()]
 
         return roi
+
+
+class Region_Proposal_Network(nn.Module):
+    def __init__(self, dev, in_channels=512, mid_channels=512, feat_stride=16, proposal_creator_params=dict()):
+        super(Region_Proposal_Network, self).__init__()
+        self.dev = dev
+        self.feat_stride = feat_stride
+        self.proposal_layer = Proposal_Creator(self, **proposal_creator_params)
+        self.anchor_base = generate_anchor()
+        n_anchor = self.anchor_base.shape[0]
+
+        self.conv1 = nn.Conv2d(in_channels, mid_channels, 3, 1, 1)
+        self.score = nn.Conv2d(mid_channels, n_anchor * 2, 1, 1, 0)
+        self.loc = nn.Conv2d(mid_channels, n_anchor * 4, 1, 1, 0)
+        weight_initialize(self.conv1, 0, 0.01)
+        weight_initialize(self.score, 0, 0.01)
+        weight_initialize(self.loc, 0, 0.01)
+
+    def forward(self, x, img_size, scale=1.):
+        # x : feature map extreaced from images -> (N, C, H, W)
+        n, _, hh, ww = x.shape
+
+        # (h*w*9) anchor box coordinates
+        anchor = _enumerate_shifted_anchor(np.array(self.anchor_base), self.feat_stride, hh, ww)
+        n_anchor = anchor.shape[0] // (hh * ww)
+        
+        hidden = F.relu(self.conv1(x))
+
+        # bounding box offset & object socres
+        rpn_locs = self.loc(hidden)
+        rpn_locs = rpn_locs.permute(0, 2, 3, 1).contiguous().view(n, -1, 4)
+
+        rpn_scores = self.socre(hidden)
+        rpn_scores = rpn_scores.permute(0, 2, 3, 1).contiguous()
+
+        rpn_softmax_scores = F.softmax(rpn_scores.view(n, hh, ww, n_anchor, 2), dim=4)
+        rpn_fg_scores = rpn_softmax_scores[:, :, :, :, 1].contiguous()
+        rpn_fg_scores = rpn_fg_scores.view(n, -1)
+
+        rpn_scores = rpn_scores.view(n, -1, 2)
+
+        # generate proposal
+        rois = list()
+        roi_indices = list() # index of image
+
+        for i in range(n):
+            roi = self.proposal_layer(self.dev, rpn_locs[i].cpu().data.numpy(), rpn_fg_scores[i].cpu().data.numpy(), anchor, img_size, scale=scale)
+            batch_index = i * np.ones((len(roi),), dtype=np.int32)
+            rois.append(roi)
+            roi_indices.append(batch_index)
+
+        rois = np.concatenate(rois, index=0)
+        roi_indices = np.concatenate(roi_indices, index=0)
+
+        return rpn_locs, rpn_scores, rois, roi_indices, anchor
+
+
+def weight_initialize(m, mean, stddev, truncated=False):
+    if truncated:
+        m.weight.data.normal_().fmod_(2).mul_(stddev).add_(mean)
+    else:
+        m.weight.data.normal_(mean, stddev)
+        m.bias.data.zero_()
+        
+
+def _enumerate_shifted_anchor(anchor_base, feat_stride, height, width):
+    '''
+    enumerate anchor boxes of all pixels
+    (generate_anchor() : 9 anchor boxes on specific pixel)
+    '''
+
+    shift_y = np.arange(0, height * feat_stride, feat_stride)
+    shift_x = np.arange(0, width * feat_stride, feat_stride)
+    shift_x, shift_y = np.meshgrid(shift_x, shift_y)
+    shift = np.stack((shift_y.ravel(), shift_x.ravel(), shift_y.ravel(), shift_x.rabel()), axis=1)
+
+    A = anchor_base.shape[0]
+    K = shift.shape[0]
+    anchor = anchor_base.reshape((1, A, 4)) + shift.reshape((1, K, 4)).transpose((1, 0, 2))
+    anchor = anchor.reshape((K * A, 4)).astype(np.float32)
+
+    return anchor
